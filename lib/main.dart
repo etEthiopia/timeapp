3cf56082899ab02f36a53a8a982ddbe482ed09a8
etEthiopia <daginegussu@gmail.com>
2020-04-07 12:49:32 +0300

    routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:timeapp/pages/choose_location.dart';
 import 'package:timeapp/pages/home.dart';
+import 'package:timeapp/pages/loading.dart';
 
 main() => runApp(MaterialApp(
-  home: Home(),
-));
-
+      routes: {
+        "/": (context) => Loading(),
+        "/home": (context) => Home(),
+        "/location": (context) => ChooseLocation()
+      },
+    ));
