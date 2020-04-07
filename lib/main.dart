5b22c3dc936959e9d79ed95c8708e7aa2ed66617
etEthiopia <daginegussu@gmail.com>
2020-04-07 13:15:11 +0300

    push and pop routes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:timeapp/pages/home.dart';
 import 'package:timeapp/pages/loading.dart';
 
 main() => runApp(MaterialApp(
+      initialRoute: "/home",
       routes: {
         "/": (context) => Loading(),
         "/home": (context) => Home(),
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,7 +9,16 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text("Home Screen")),
+      body: SafeArea(
+        child: Column(children: <Widget>[
+          RaisedButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, '/location');
+              },
+              icon: Icon(Icons.location_on),
+              label: Text("Choose Location"))
+        ]),
+      ),
     );
   }
 }
